01b4dae097e1d2faca43f148bae32b811bf47746
lucia <lcec3@alu.ua.es>
2025-03-21 13:54:18 +0100

    48784005Q - libreria creada

--- hada-p3/library/Properties/AssemblyInfo.cs
+++ hada-p3/library/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
-// asociada a un ensamblado.
+// asociada con un ensamblado.
 [assembly: AssemblyTitle("library")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("96d304ae-360b-4718-9921-0ca8e9280caa")]
+[assembly: Guid("dc30c27d-9533-4763-ab19-690743ab2496")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
