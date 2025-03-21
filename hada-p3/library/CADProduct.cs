3c9c363ed69fd64ec91d0505aaf198c34f362536
lucia <lcec3@alu.ua.es>
2025-03-21 14:24:16 +0100

    48784005Q - CADProduct() y metodos Create y update, ENProduct clase.

--- hada-p3/library/CADProduct.cs
+++ hada-p3/library/CADProduct.cs
@@ -3,15 +3,92 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace library
 {
     public class CADProduct
     {
-        public CADProduct() { 
-        
+        private string constring;
+        public CADProduct() 
+        { 
+            constring = System.Configuration.ConfigurationManager.ConnectionStrings["DatabaseConnectionString"].ConnectionString;
         }
         public bool Create(ENProduct en)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(constring))
+                {
+                    string query = "INSERT INTO Products (code, name, amount, price, category, creationDate) VALUES (@code, @name, @amount, @price, @category, @creationDate)";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@code", en.code);
+                    command.Parameters.AddWithValue("@name", en.name);
+                    command.Parameters.AddWithValue("@amount", en.amount);
+                    command.Parameters.AddWithValue("@price", en.price);
+                    command.Parameters.AddWithValue("@category", en.category);
+                    command.Parameters.AddWithValue("@creationDate", en.CreationDate);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+                return false;
+            }
+        }
+
+        public bool Update(ENProduct en)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(constring))
+                {
+                    string query = "UPDATE Products SET name = @name, amount = @amount, price = @price, category = @category, creationDate = @creationDate WHERE code = @code";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@code", en.code);
+                    command.Parameters.AddWithValue("@name", en.name);
+                    command.Parameters.AddWithValue("@amount", en.amount);
+                    command.Parameters.AddWithValue("@price", en.price);
+                    command.Parameters.AddWithValue("@category", en.category);
+                    command.Parameters.AddWithValue("@creationDate", en.CreationDate);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+                return false;
+            }
+        }
+
+        public bool Delete(ENProduct en) 
+        { 
+        
+        }
+
+        public bool Read(ENProduct en) 
+        {
+
+        }
+
+        public bool ReadFirst(ENProduct en)
+        {
+
+        }
+
+        public bool ReadNext(ENProduct en)
+        {
+
+        }
+
+        public bool ReadPrev(ENProduct en)
         {
 
         }
--- hada-p3/library/ENProduct.cs
+++ hada-p3/library/ENProduct.cs
@@ -7,8 +7,57 @@ using System.Threading.Tasks;
 
 namespace library
 {
-    internal class ENProduct
+    public class ENProduct
     {
-        private 
+        private string code; 
+        private string name;
+        private int amount; 
+        private float price;
+        private int category;
+        private DateTime creationDate; 
+        public ENProduct()
+        {
+
+        }
+
+        public ENProduct(string code, string name, int amount, float price, int category, DateTime creationDate)
+        {
+
+        }
+
+        public bool Create()
+        {
+
+        }
+
+        public bool Update()
+        {
+
+        }
+
+        public bool Delete()
+        {
+
+        }
+
+        public bool Read()
+        {
+
+        }
+
+        public bool ReadFirst()
+        {
+
+        }
+
+        public bool ReadNext()
+        {
+
+        }
+
+        public bool ReadPrev()
+        {
+
+        }
     }
 }
